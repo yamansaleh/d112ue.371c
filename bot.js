db0cba0af62546b67d76c11489adee056aee4bb3
yamansaleh <57991948+yamansaleh@users.noreply.github.com>
2019-12-08 15:05:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame("الستريم هنا", "https://www.twitch.tv/Alpha");
+ client.user.setGame("Hi", "https://www.twitch.tv/Alpha");
 });
  
  
